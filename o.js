730cc13362dd5de4aed6a7472c6158c169624d1b
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-09 15:35:23 +0300

    Update o.js

--- o.js
+++ o.js
@@ -402,14 +402,18 @@ client.on('message', msg => {
 
 
 
-if (msg.content ===  prefix + 'havadurumuankara') { 
+if (msg.content ===  prefix + 'havadurumu') { 
 var weather = require('weather-js');
+ 
+// Options:
+// search:     location name or zipcode
+// degreeType: F or C
+ 
 weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
   if(err) console.log(err);
  
   console.log(JSON.stringify(result, null, 2));
 });
-}
 });
 
 //msg.channel.delete() kanalı siliyor
